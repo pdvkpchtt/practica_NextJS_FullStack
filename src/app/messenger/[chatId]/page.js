45f8b5e514476b89b6f2679a35f4550ee7dbeaca
Danil <kabirdanilov14@gmail.com>
2023-11-12 22:23:01 +0500

    donewith right messenger

--- src/app/messenger/[chatId]/page.js
+++ src/app/messenger/[chatId]/page.js
@@ -1,5 +1,4 @@
 import ChatParent from "../../../components/messenger/ChatParent";
-import ChatsPanel from "../../../components/messenger/ChatsPanel";
 
 const TestChatPage = async ({ params: { chatId }, searchParams }) => {
   const user_id = searchParams?.user_id;
