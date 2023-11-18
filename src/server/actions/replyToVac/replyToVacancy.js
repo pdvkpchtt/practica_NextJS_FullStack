403b2ccc32beebe27be1a8617ed313c86634738d
Danil <kabirdanilov14@gmail.com>
2023-11-19 01:08:16 +0500

    testing of zod

--- src/server/actions/replyToVac/replyToVacancy.js
+++ src/server/actions/replyToVac/replyToVacancy.js
@@ -2,147 +2,161 @@
 
 import { getServSession } from "app/api/auth/[...nextauth]/route";
 import { prisma } from "../../db";
+import { z } from "zod";
 
 export const replyToVacancy = async (vacId, link, message) => {
   const session = await getServSession();
 
-  const vacReply = await prisma.VacancyReply.create({
-    data: {
-      user: {
-        connect: {
-          id: session?.user?.id,
-        },
-      },
-      vacancy: {
-        connect: {
-          id: vacId,
-        },
-      },
-      message: message,
-      link: link,
-    },
-    select: {
-      id: true,
-    },
+  const validate = z.object({
+    link: z
+      .string()
+      .url({ message: "Требуется ссылка" })
+      .min(1, { message: "Поле обязательно для заполнения" }),
+    message: z.string().min(1, { message: "Поле обязательно для заполнения" }),
   });
 
-  const files = await prisma.File.findMany({
-    where: {
-      AND: [
-        {
-          vacancyReplyId: null,
-        },
-        { user: { id: session?.user?.id } },
-      ],
-    },
-    select: {
-      id: true,
-    },
-  });
+  console.log(
+    validate.safeParse({ link, message }).error?.errors?.map((i) => i?.message),
+    "validate"
+  );
 
-  for (const file of files) {
-    await prisma.File.update({
-      where: {
-        id: file.id,
-      },
-      data: {
-        vacancyReply: {
-          connect: {
-            id: vacReply.id,
-          },
-        },
-      },
-    });
-  }
+  // const vacReply = await prisma.VacancyReply.create({
+  //   data: {
+  //     user: {
+  //       connect: {
+  //         id: session?.user?.id,
+  //       },
+  //     },
+  //     vacancy: {
+  //       connect: {
+  //         id: vacId,
+  //       },
+  //     },
+  //     message: message,
+  //     link: link,
+  //   },
+  //   select: {
+  //     id: true,
+  //   },
+  // });
 
-  const hrCreator = await prisma.Vacancy.findUnique({
-    where: { id: vacId },
-    select: {
-      hrCreator: {
-        select: {
-          userId: true,
-        },
-      },
-    },
-  });
+  // const files = await prisma.File.findMany({
+  //   where: {
+  //     AND: [
+  //       {
+  //         vacancyReplyId: null,
+  //       },
+  //       { user: { id: session?.user?.id } },
+  //     ],
+  //   },
+  //   select: {
+  //     id: true,
+  //   },
+  // });
 
-  ///дальше хз как
-  ///надо достать chatId с hrCreator и там создать message, который будет сконнекчен с vacReply.id
-  ///ща я сделаю
-  // проверяем, если чат с hrCreator'ом есть
-  const foundChat = await prisma.chat.findFirst({
-    where: {
-      AND: [
-        {
-          participants: { some: { id: session?.user?.id } },
-        },
-        {
-          participants: { some: { id: hrCreator?.hrCreator?.userId } },
-        },
-      ],
-    },
-    select: { id: true, participants: true },
-  });
+  // for (const file of files) {
+  //   await prisma.File.update({
+  //     where: {
+  //       id: file.id,
+  //     },
+  //     data: {
+  //       vacancyReply: {
+  //         connect: {
+  //           id: vacReply.id,
+  //         },
+  //       },
+  //     },
+  //   });
+  // }
 
-  // если нет, тосоздаём чат
-  if (!foundChat?.id) {
-    const chat = await prisma.chat.create({
-      data: {
-        participants: {
-          connect: [
-            {
-              id: session?.user?.id,
-            },
-            { id: hrCreator?.hrCreator?.userId },
-          ],
-        },
-      },
-    });
+  // const hrCreator = await prisma.Vacancy.findUnique({
+  //   where: { id: vacId },
+  //   select: {
+  //     hrCreator: {
+  //       select: {
+  //         userId: true,
+  //       },
+  //     },
+  //   },
+  // });
 
-    const newmessage = await prisma.message.create({
-      data: {
-        text: "Отклик на вакансию",
-        type: "vacancyReply",
-        unRead: true,
-        Chat: {
-          connect: { id: chat?.id },
-        },
-        User: {
-          connect: { id: session?.user?.id },
-        },
-        vacancyReply: { connect: { id: vacReply.id } },
-      },
-    });
+  // ///дальше хз как
+  // ///надо достать chatId с hrCreator и там создать message, который будет сконнекчен с vacReply.id
+  // ///ща я сделаю
+  // // проверяем, если чат с hrCreator'ом есть
+  // const foundChat = await prisma.chat.findFirst({
+  //   where: {
+  //     AND: [
+  //       {
+  //         participants: { some: { id: session?.user?.id } },
+  //       },
+  //       {
+  //         participants: { some: { id: hrCreator?.hrCreator?.userId } },
+  //       },
+  //     ],
+  //   },
+  //   select: { id: true, participants: true },
+  // });
 
-    const updchat = await prisma.chat.update({
-      where: { id: chat.id },
-      data: {
-        updatedAt: new Date(),
-      },
-    });
-  } else {
-    const newmessage = await prisma.message.create({
-      data: {
-        text: "Отклик на вакансию",
-        type: "vacancyReply",
-        unRead: true,
-        Chat: {
-          connect: { id: foundChat.id },
-        },
-        User: {
-          connect: { id: session?.user?.id },
-        },
-        vacancyReply: { connect: { id: vacReply.id } },
-      },
-    });
+  // // если нет, тосоздаём чат
+  // if (!foundChat?.id) {
+  //   const chat = await prisma.chat.create({
+  //     data: {
+  //       participants: {
+  //         connect: [
+  //           {
+  //             id: session?.user?.id,
+  //           },
+  //           { id: hrCreator?.hrCreator?.userId },
+  //         ],
+  //       },
+  //     },
+  //   });
 
-    const chat = await prisma.chat.update({
-      where: { id: foundChat.id },
-      data: {
-        updatedAt: new Date(),
-      },
-    });
-  }
+  //   const newmessage = await prisma.message.create({
+  //     data: {
+  //       text: "Отклик на вакансию",
+  //       type: "vacancyReply",
+  //       unRead: true,
+  //       Chat: {
+  //         connect: { id: chat?.id },
+  //       },
+  //       User: {
+  //         connect: { id: session?.user?.id },
+  //       },
+  //       vacancyReply: { connect: { id: vacReply.id } },
+  //     },
+  //   });
+
+  //   const updchat = await prisma.chat.update({
+  //     where: { id: chat.id },
+  //     data: {
+  //       updatedAt: new Date(),
+  //     },
+  //   });
+  // } else {
+  //   const newmessage = await prisma.message.create({
+  //     data: {
+  //       text: "Отклик на вакансию",
+  //       type: "vacancyReply",
+  //       unRead: true,
+  //       Chat: {
+  //         connect: { id: foundChat.id },
+  //       },
+  //       User: {
+  //         connect: { id: session?.user?.id },
+  //       },
+  //       vacancyReply: { connect: { id: vacReply.id } },
+  //     },
+  //   });
+
+  //   const chat = await prisma.chat.update({
+  //     where: { id: foundChat.id },
+  //     data: {
+  //       updatedAt: new Date(),
+  //     },
+  //   });
+  // }
 
   // console.log("digitstestomg", foundChat, hrCreator?.hrCreator?.userId);s
 };
--- src/shared/icons/AddCityIcon.js
+++ src/shared/icons/AddCityIcon.js
@@ -1,41 +1,5 @@
 const AddCityIcon = ({ onClick = () => {}, area = false }) => {
   return (
-    // <svg
-    //   width="18"
-    //   height="18"
-    //   viewBox="0 0 18 18"
-    //   fill="none"
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   className="cursor-pointer"
-    //   onClick={onClick}
-    // >
-    //   <path
-    //     d="M9 15.75C9.88642 15.75 10.7642 15.5754 11.5831 15.2362C12.4021 14.897 13.1462 14.3998 13.773 13.773C14.3998 13.1462 14.897 12.4021 15.2362 11.5831C15.5754 10.7642 15.75 9.88642 15.75 9C15.75 8.11358 15.5754 7.23583 15.2362 6.41689C14.897 5.59794 14.3998 4.85382 13.773 4.22703C13.1462 3.60023 12.4021 3.10303 11.5831 2.76381C10.7642 2.42459 9.88642 2.25 9 2.25C7.20979 2.25 5.4929 2.96116 4.22703 4.22703C2.96116 5.4929 2.25 7.20979 2.25 9C2.25 10.7902 2.96116 12.5071 4.22703 13.773C5.4929 15.0388 7.20979 15.75 9 15.75Z"
-    //     className={
-    //       area
-    //         ? "stroke-[#687094] hover:stroke-[#51597A] active:stroke-[#444B67] fill-[#687094] hover:fill-[#51597A] active:fill-[#444B67] transition duration-[250ms]"
-    //         : "stroke-[#5875e8] hover:stroke-[#3A56C5] active:stroke-[#2C429C] fill-[#5875e8] hover:fill-[#3A56C5] active:fill-[#2C429C] transition duration-[250ms]"
-    //     }
-    //     strokeWidth="1.5"
-    //     strokeLinecap="round"
-    //     strokeLinejoin="round"
-    //   />
-    //   <path
-    //     d="M6 9H12"
-    //     stroke="white"
-    //     strokeWidth="1.2"
-    //     strokeLinecap="round"
-    //     strokeLinejoin="round"
-    //   />
-    //   <path
-    //     d="M9 12V6"
-    //     stroke="white"
-    //     strokeWidth="1.2"
-    //     strokeLinecap="round"
-    //     strokeLinejoin="round"
-    //   />
-    // </svg>
-
     <svg
       width="24"
       height="24"
@@ -59,14 +23,14 @@ const AddCityIcon = ({ onClick = () => {}, area = false }) => {
       <path
         d="M8 12H16"
         stroke="white"
-        strokeWidth="1.2"
+        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M12 16V8"
         stroke="white"
-        strokeWidth="1.2"
+        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
