c9dc63d91b712fe4c43956c084a40fa434610c8c
Danil <kabirdanilov14@gmail.com>
2023-11-28 11:29:51 +0500

    jops

--- src/server/actions/messenger/getUnreadMessagesCount.js
+++ src/server/actions/messenger/getUnreadMessagesCount.js
@@ -1,27 +1,29 @@
-'use server'
-import { prisma } from '../../db'
-import { getServSession } from 'app/api/auth/[...nextauth]/route'
+"use server";
+import { prisma } from "../../db";
+import { getServSession } from "app/api/auth/[...nextauth]/route";
 
-const getUnreadMessagesCount = async lastDate => {
-	const session = await getServSession()
-	let totalUnread = 0
+const getUnreadMessagesCount = async (lastDate) => {
+  const session = await getServSession();
+  let totalUnread = 0;
 
-	const chats = await prisma.message.groupBy({
-		by: ['chatId'],
-		_count: { unRead: true },
-		where: {
-			userId: { not: session.user.id },
-			unRead: true,
-			Chat: {
-				updatedAt: { lte: new Date().toISOString() },
-				participants: { some: { id: session.user.id } },
-			},
-		},
-	})
-	for (let chat of chats) {
-		totalUnread += chat._count.unRead
-	}
+  const chats = await prisma.message.groupBy({
+    by: ["chatId"],
+    _count: { unRead: true },
+    where: {
+      userId: { not: session.user.id },
+      unRead: true,
+      Chat: {
+        // updatedAt: { lte: new Date().toISOString() },
+        participants: { some: { id: session.user.id } },
+      },
+    },
+  });
+  for (let chat of chats) {
+    totalUnread += chat._count.unRead;
+  }
 
-	return { totalUnread }
-}
-export default getUnreadMessagesCount
+  console.log(totalUnread);
+
+  return { totalUnread };
+};
+export default getUnreadMessagesCount;
