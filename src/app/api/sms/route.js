79da1fbb89d98f5c8892236957d4f348243784ce
Danil <kabirdanilov14@gmail.com>
2024-02-04 14:23:17 +0500

    юз сервер

--- src/app/api/sms/route.js
+++ src/app/api/sms/route.js
@@ -1,15 +1,12 @@
+"use server";
+
 import axios from "axios";
 
 export default async function sendVerificationCodeSMS(phone, code = null) {
   try {
-    const data = await axios.post("https://smsc.ru/sys/send.php", {
-      params: {
-        login: "Nikolay_Third",
-        psw: "ps4VSxboxone",
-        phones: "79093474343",
-        mes: "Ваш ключ к успеху: " + code,
-      },
-    });
+    const data = await axios.post(
+      "https://smsc.ru/sys/send.php?login=Nikolay_Third&psw=ps4VSxboxone&phones=79173846451&mes=Привет"
+    );
 
     return data.data;
   } catch (err) {
